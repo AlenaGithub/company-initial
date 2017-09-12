4ee986f3e5cc9114fed88f9daa98a3d9bf23fe08
lihua.li <lihua.li@CGI-D32298.ams.com>
2017-09-12 13:32:59 -0600

    update data source

--- src/main/java/io/rscale/training/company/DataSourceConfig.java
+++ src/main/java/io/rscale/training/company/DataSourceConfig.java
@@ -1,60 +1,52 @@
 package io.rscale.training.company;
 
-import javax.annotation.PostConstruct;
+import java.sql.SQLException;
+
 import javax.sql.DataSource;
 
 import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.UnsatisfiedDependencyException;
+import org.springframework.cloud.config.java.AbstractCloudConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
-
 @Configuration
-public class DataSourceConfig {
+public class DataSourceConfig extends AbstractCloudConfig {
 
-    private static final Logger logger = Logger.getLogger(DataSourceConfig.class);
+	private static final Logger logger = Logger.getLogger(DataSourceConfig.class);
 
-    @Value("${mysql.dbName}")
-    private String mysqlDbName;
-    
-    @Value("${mysql.user}")
-    private String mysqlUser;
-    
-    @Value("${mysql.password}")
-    private String mysqlPassword;
-    
-    @Value("${mysql.server}")
-    private String mysqlServer;
-    
-    @Value("${mysql.port}")
-    private int mysqlPort;
-    
-    private DataSource dataSource;
-    
-    public DataSourceConfig() {}
-    
-    @PostConstruct
-    public void init() {
-        logger.info(this.getClass() + " loaded");
-        try {
-	    	MysqlDataSource mysql = new MysqlDataSource();
-	    	mysql.setDatabaseName(mysqlDbName);
-	    	mysql.setUser(mysqlUser);
-	    	mysql.setPassword(mysqlPassword);
-	    	mysql.setServerName(mysqlServer);
-	    	mysql.setPort(mysqlPort);
-	    	logger.info("Created datasource:  " + mysql);
-	        this.dataSource = mysql;
-    	} catch (Exception e) {
-    		logger.error("Could not create datasoruce", e);
-    	}
-        logger.info(this.getClass() + " initialized");
-    }
+	public DataSourceConfig() {
+		logger.info(this.getClass() + " loaded");
+	}
 
-    @Bean
-    public DataSource dataSource() {
-    	return dataSource;
-    }
+	@Bean
+	public DataSource dataSource() throws Exception {
+
+		DataSource dataSource = cloud().getSingletonServiceConnector(DataSource.class, null);
+		if (!isMySQL(dataSource)) {
+			logger.error("MySQL required when running cloud profile.");
+			throw new UnsatisfiedDependencyException("javax.sql.DataSource", "javax.sql.DataSource",
+					"javax.sql.DataSource", "MySQL required when running cloud profile.");
+		}
+		return dataSource;
+
+	}
+
+	private boolean isMySQL(DataSource dataSource) {
+		
+		String driverName = null;
+		boolean flag = false;
+		try {
+			driverName = dataSource.getConnection().getMetaData().getDriverName();
+			if (driverName.equalsIgnoreCase("mysql")){
+				flag = true;
+			}
+			System.out.println(">>>>>>>>>>>" + driverName);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return flag;
+		
+	}
 
 }
